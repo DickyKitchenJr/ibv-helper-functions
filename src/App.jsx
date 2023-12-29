1ec17b1960f46a39f7f7bd73c851fa9c3a4815c7
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-12-29 14:17:06 -0500

    Update App.jsx
    experimenting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import './App.css';
+import { bioForJson } from './components/Bio-Formatting/bioForJson';
+import { bioForListing } from './components/Bio-Formatting/bioForListing';
+import { Authors } from './assets/Authors';
 
 function App() {
 
+  let authorsToProcess = Authors
+
+  let authorListings;
+
+  const processAuthors = (listOfAuthors) => {
+    while(listOfAuthors.length > 0){
+      let currentAuthor = listOfAuthors[0];
+      currentAuthor.bio = bioForJson(currentAuthor["bio"]);
+      authorListings.push(currentAuthor);  
+      console.log(authorListings[0].firstName)
+      listOfAuthors.shift();
+    }
+  }
+
   return (
     <>
-      Here I am
+      {console.log(processAuthors(Authors))}
+      <h1>Testing Area</h1>
+      {/* converting author array bio into single string */}
+      <h2>Bio</h2>
+      <p>{bioForJson(Authors[2].bio)}</p>
+      <p>{Authors[2].bio}</p>
     </>
-  )
+  );
 }
 
 export default App
