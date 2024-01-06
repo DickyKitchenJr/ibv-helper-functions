0c538e4759cb1b4c55b05328a3c963f9d9411cf0
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-01-06 13:03:35 -0500

    Create DatabaseAuthors.jsx
    working on fetching data from database and showing it on the screen, currently not working as expected

---
+++ src/components/DatabaseAuthors/DatabaseAuthors.jsx
@@ -0,0 +1,147 @@
+//setting up fetch request and populating authorsInDatabase before rendering DatabaseAuthors
+const apiAddress = import.meta.env.VITE_API_ADDRESS;
+const authorsInDatabase = [];
+
+// const fetchData = (url) => {
+//    fetch(url)
+//     console.log('fetching')
+//     .then(response => {
+//         if(!response.ok) {
+//             throw new Error(`Bad Response: ${response.status}`);
+//         }
+//         console.log('fetch successful')
+//         return response.json();
+//     })
+//     .then(data => {
+//         authorsInDatabase.push(data.authors)
+//     })
+//     .catch(error =>{
+//         console.error('Error:', error);
+//     });
+// };
+
+const fetchData = (url) => {
+  console.log("fetching");
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Bad Response: ${response.status}`);
+      }
+      console.log("fetch successful");
+      return response.json();
+    })
+    .then((data) => {
+      authorsInDatabase.push(data.authors);
+      console.log(authorsInDatabase)
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+fetchData(apiAddress).then(()=>{return authorsInDatabase});
+
+function DatabaseAuthors() {
+
+  return (
+    <>
+      <h2>Authors Currently In Database</h2>
+      <p>{authorsInDatabase[0].id}</p>
+      {authorsInDatabase.map((author) => {
+        return (
+          <>
+            <p>
+              &#123;
+              <br />
+              firstName: &#34;{author.firstName}&#34;,
+              <br />
+              lastName: &#34;{author.lastName}&#34;,
+              <br />
+              {author.website ? (
+                <>
+                  website &#34;{author.website}&#34;,
+                  <br />
+                </>
+              ) : null}
+              umbrellaGenre: [
+              {author.umbrellaGenre.map((each, index, array) => (
+                <>
+                  &#34;{each}&#34;{index === array.length - 1 ? "" : ","}
+                </>
+              ))}
+              ],
+              <br />
+              subGenre: [
+              {author.subGenre.map((each, index, array) => (
+                <>
+                  &#34;{each}&#34;{index === array.length - 1 ? "" : ","}
+                </>
+              ))}
+              ],
+              <br />
+              {author.instagram ? (
+                <>
+                  instagram: &#34;{author.instagram}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.facebook ? (
+                <>
+                  facebook: &#34;{author.facebook}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.twitter ? (
+                <>
+                  twitter: &#34;{author.twitter}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.tiktok ? (
+                <>
+                  tiktok: &#34;{author.tiktok}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.goodreads ? (
+                <>
+                  goodreads: &#34;{author.goodreads}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.mastodon ? (
+                <>
+                  mastodon: &#34;{author.mastodon}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.amazonBio ? (
+                <>
+                  amazonBio: &#34;{author.amazonBio}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.threads ? (
+                <>
+                  threads: &#34;{author.threads}&#34;,
+                  <br />
+                </>
+              ) : null}
+              {author.bookbub ? (
+                <>
+                  bookbub: &#34;{author.bookbub}&#34;,
+                  <br />
+                </>
+              ) : null}
+              bio: &#34;{author.bio}&#34;
+              <br />
+              &#125;,
+            </p>
+          </>
+        );
+      })}
+    </>
+  );
+}
+
+export default DatabaseAuthors;
