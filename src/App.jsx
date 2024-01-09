cdf54e52495b2a4437c6bc7eb8ddc46c4938f569
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-01-08 20:05:37 -0500

    Update App.jsx
    - handled fetching data in App so that it to separate the logic of getting the data and using it in DatabaseAuthors
    
    - conditionally rendered DatabaseAuthors only when data is present (length > 0)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import AuthorsToProcess from './components/AuthorsToProcess/AuthorsToProcess';
 import DatabaseAuthors from './components/DatabaseAuthors/DatabaseAuthors';
 
 function App() {
-  
+  const [data, setData] = useState([]);
+  const apiAddress = import.meta.env.VITE_API_ADDRESS;
+
+  useEffect(() => {
+    const fetchData = (url) => {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Bad Response: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((dataFetched) => {
+         setData(dataFetched.authors);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    };
+
+    fetchData(apiAddress)
+  }, [])
 
   return (
     <>
       <h1>Testing Area</h1>
-      <DatabaseAuthors />
+      {data.length === 0 ? null : <DatabaseAuthors data = {data} />}
       {/* <AuthorsToProcess /> */}
     </>
   );
