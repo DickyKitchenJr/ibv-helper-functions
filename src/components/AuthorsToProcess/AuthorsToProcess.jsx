d10073d76dba8fd04afd90dff29e9a7ed0eb0875
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-01-12 15:33:09 -0500

    Update AuthorsToProcess.jsx

--- src/components/AuthorsToProcess/AuthorsToProcess.jsx
+++ src/components/AuthorsToProcess/AuthorsToProcess.jsx
@@ -1,62 +1,62 @@
 import { Authors } from "../../assets/Authors";
 import { bioForJson } from "../Bio-Formatting/bioForJson";
 import { socialMediaExtractor } from "../SocialMedia-Formatting/socialMediaExtractor";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// // create a copy of Authors to modify that can be compared with the original Authors list if needed
-// let authorsToProcess = Authors
+// create a copy of Authors to modify that can be compared with the original Authors list if needed
+let authorsToProcess = Authors
 
-// const authorListings = [];
+const authorListings = [];
 
-// // create a function that goes through the entire authorsToProcess list and updates the bio and extracts everything from socialMedia
-// const processAuthors = (listOfAuthors) => {
-//     while(listOfAuthors.length > 0){
-//       let currentAuthor = listOfAuthors[0];
-//       socialMediaExtractor(currentAuthor);
-//       currentAuthor.bio = bioForJson(currentAuthor["bio"]);
-//       authorListings.push(currentAuthor);  
-//       listOfAuthors.shift();
-//     }
-//     return authorListings;
-//   }
+// create a function that goes through the entire authorsToProcess list and updates the bio and extracts everything from socialMedia
+const processAuthors = (listOfAuthors) => {
+    while(listOfAuthors.length > 0){
+      let currentAuthor = listOfAuthors[0];
+      // socialMediaExtractor(currentAuthor); To be used if links are in "socialMedia" object; not to be used if listed individually
+      // currentAuthor.bio = bioForJson(currentAuthor["bio"]); To be used if bio is stored in an array (also addresses double quotes)
+      authorListings.push(currentAuthor);  
+      listOfAuthors.shift();
+    }
+    return authorListings;
+  }
 
-// // call processAuthors on authorsToProcess to create the list for use in App  
-// processAuthors(authorsToProcess);
+// call processAuthors on authorsToProcess to create the list for use in App  
+processAuthors(authorsToProcess);
 
 function AuthorsToProcess() {
   // Used in loading authors currently listed on the front-end onto the database. Will it be used again? Unsure at this time.
-  // useEffect(() => {
-  //   async function sendPostRequest(data) {
-  //     const apiAddress = import.meta.env.VITE_API_ADDRESS;
+  useEffect(() => {
+    async function sendPostRequest(data) {
+      const apiAddress = import.meta.env.VITE_API_ADDRESS;
 
-  //     try {
-  //       const response = await fetch(apiAddress, {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(data),
-  //       });
+      try {
+        const response = await fetch(apiAddress, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
 
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
 
-  //       const responseData = await response.json();
-  //       console.log("Post request successful:", responseData);
-  //     } catch (error) {
-  //       console.error("Error sending post request:", error);
-  //     }
-  //   }
+        const responseData = await response.json();
+        console.log("Post request successful:", responseData);
+      } catch (error) {
+        console.error("Error sending post request:", error);
+      }
+    }
 
-  //   async function sendArraySequentially() {
-  //     for (const item of authorListings) {
-  //       await sendPostRequest(item);
-  //     }
-  //   }
-  //   // Trigger the array processing when the component mounts
-  //   sendArraySequentially();
-  // }, []);
+    async function sendArraySequentially() {
+      for (const item of authorListings) {
+        await sendPostRequest(item);
+      }
+    }
+    // Trigger the array processing when the component mounts
+    sendArraySequentially();
+  }, []);
 
   return (
     <>
